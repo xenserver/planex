59c4a2604c2baf66512d186771a7e97fd858151c
Jon Ludlam <jonathan.ludlam@eu.citrix.com>
2013-07-02 23:38:29 +0100

    Add some XenServer build system integration glue
    Signed-off-by: Jon Ludlam <jonathan.ludlam@eu.citrix.com>

---
+++ mk/get_config_dir.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+
+import os
+
+component = os.getenv("COMPONENT")
+
+if component == "ocaml":
+    print "/repos/xen-dist-ocaml.hg"
+if component == "api-libs":
+    print "/repos/xen-api-libs-rpm-buildroot"
+
