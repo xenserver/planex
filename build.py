200a6b9b3f9bd140bbbb2bc45d5ceea488acf336
Si Beaumont <simon.beaumont@citrix.com>
2013-07-03 14:48:13 +0100

    Formatting: Conform to PEP-8 guidelines

--- build.py
+++ build.py
@@ -2,37 +2,46 @@
 
 # Build a bunch of SRPMs
 
-import os, glob, subprocess, sys
-import rpm
-import demjson
+import sys
+import os
+import glob
+import subprocess
 import shutil
+
+import demjson
+
+SRPMS_DIR = "SRPMS"
+RPMS_DIR = "RPMS"
+BUILD_DIR = "BUILD"
+TMP_RPM_PATH = "/tmp/RPMS"
 
 
 class RpmError(Exception):
-	pass
- 
-srpmspath = "SRPMS"
-rpmspath = "RPMS"
-buildpath = "BUILD"
-tmp_path = "/tmp/RPMS"
+    pass
+
 
 def exists(path):
-    return os.access(path,os.F_OK)
+    return os.access(path, os.F_OK)
+
 
 def doexec(args, inputtext=None):
     """Execute a subprocess, then return its return code, stdout and stderr"""
     print "Executing: %s" % args
-    proc = subprocess.Popen(args,stdin=subprocess.PIPE,stdout=subprocess.PIPE,stderr=subprocess.PIPE,close_fds=True)
-    (stdout,stderr) = proc.communicate(inputtext)
+    proc = subprocess.Popen(args, stdin=subprocess.PIPE,
+                            stdout=subprocess.PIPE, stderr=subprocess.PIPE,
+                            close_fds=True)
+    (stdout, stderr) = proc.communicate(inputtext)
     rc = proc.returncode
-    return (rc,stdout,stderr)
+    return (rc, stdout, stderr)
 
-def run_srpmutil(specfile,srpm):
-    for x in ['i686','i386','noarch']:
-	(rc,stdout,stderr) = doexec(["srpmutil/srpmutil",specfile,srpm,x])
-	if(rc==0):
-	    return (stdout,x)
+
+def run_srpmutil(specfile, srpm):
+    for x in ['i686', 'i386', 'noarch']:
+        (rc, stdout, stderr) = doexec(["srpmutil/srpmutil", specfile, srpm, x])
+        if rc == 0:
+            return (stdout, x)
     raise RpmError
+
 
 def get_srpm_info(srpm):
     """ Returns a dictionary of interesting info about an SRPM:
@@ -66,7 +75,7 @@ def get_srpm_info(srpm):
 
     """
     if not os.path.exists("srpmutil/srpmutil"):
-        (rc,stdout,stderr) = doexec(["make","-C","srpmutil"])
+        (rc, stdout, stderr) = doexec(["make", "-C", "srpmutil"])
         if rc != 0:
             print "Error while building srpmutil:"
             print "stdout\n======\n%s\n\n" % stdout
@@ -75,20 +84,23 @@ def get_srpm_info(srpm):
 
     for x in glob.glob("SPECS/*.spec"):
         os.unlink(x)
-    (rc,stdout,stderr) = doexec(["rpm","-i",srpm])
+    (rc, stdout, stderr) = doexec(["rpm", "-i", srpm])
     myspecfile = glob.glob("./SPECS/*.spec")[0]
     try:
-	(specfile,arch) = run_srpmutil(myspecfile,srpm) 
-	j = demjson.decode(specfile)
-	(rc,stdout,stderr) = doexec(["rpm","-qp",srpm,"-R"])
-	lines = stdout.split('\n')
-	alldeps = map(lambda x: x.split(' ')[0], lines)
-	realdeps = filter(lambda x: len(x) <> 0 and x<>"rpmlib(CompressedFileNames)",alldeps)
-	j['deps']=realdeps
-	j['arch']=arch
-	return j
+        (specfile, arch) = run_srpmutil(myspecfile, srpm)
+        j = demjson.decode(specfile)
+        (rc, stdout, stderr) = doexec(["rpm", "-qp", srpm, "-R"])
+        lines = stdout.split('\n')
+        alldeps = map(lambda x: x.split(' ')[0], lines)
+        realdeps = filter(
+            lambda x: len(x) and x != "rpmlib(CompressedFileNames)",
+            alldeps)
+        j['deps'] = realdeps
+        j['arch'] = arch
+        return j
     except:
-	return {'broken':True,'srcrpm':srpm}
+        return {'broken': True, 'srcrpm': srpm}
+
 
 def extract_target(srpm_infos, srpm_filename):
     """
@@ -98,6 +110,7 @@ def extract_target(srpm_infos, srpm_filename):
         if srpm_info["srcrpm"] == srpm_filename:
             return srpm_info["arch"]
 
+
 def get_package_to_srpm_map(srpm_info):
     m = {}
     for srpm in srpm_info:
@@ -105,26 +118,29 @@ def get_package_to_srpm_map(srpm_info):
             m[package['name']] = srpm['srcrpm']
     return m
 
+
 def get_deps(srpm_info):
     p_to_s_map = get_package_to_srpm_map(srpm_info)
     deps = {}
     for srpm in srpm_info:
         deps[srpm['srcrpm']] = set()
         for dep in srpm['deps']:
-            if p_to_s_map.has_key(dep):
+            if dep in p_to_s_map:
                 deps[srpm['srcrpm']].add(p_to_s_map[dep])
     return deps
+
 
 def toposort2(data):
     # Ignore self dependencies.
     for k, v in data.items():
         v.discard(k)
     # Find all items that don't depend on anything.
-    extra_items_in_deps = reduce(set.union, data.itervalues()) - set(data.iterkeys())
+    extra_items_in_deps = reduce(set.union,
+                                 data.itervalues()) - set(data.iterkeys())
     # Add empty dependences where needed
     extra = {}
     for item in extra_items_in_deps:
-        extra[item]=set()
+        extra[item] = set()
     data.update(extra)
     result = []
     while True:
@@ -137,8 +153,10 @@ def toposort2(data):
             if item not in ordered:
                 newdata[item] = (dep - ordered)
         data = newdata
-    assert not data, "Cyclic dependencies exist among these items:\n%s" % '\n'.join(repr(x) for x in data.iteritems())
+    assert not data, ("Cyclic dependencies exist among these items:\n%s" %
+                      '\n'.join(repr(x) for x in data.iteritems()))
     return result
+
 
 def write_rpmmacros():
     f = open('.rpmmacros', 'w')
@@ -146,13 +164,13 @@ def write_rpmmacros():
     f.close()
 
 if __name__ == "__main__":
-    packages = glob.glob( os.path.join(srpmspath, '*.src.rpm'))
+    packages = glob.glob(os.path.join(SRPMS_DIR, '*.src.rpm'))
     write_rpmmacros()
     srpm_infos = map(get_srpm_info, packages)
     deps = get_deps(srpm_infos)
     order = toposort2(deps)
 
-    for path in [tmp_path, buildpath, srpmspath, rpmspath]: 
+    for path in [TMP_RPM_PATH, BUILD_DIR, SRPMS_DIR, RPMS_DIR]:
         if not os.path.exists(path):
             os.makedirs(path)
 
@@ -161,21 +179,21 @@ if __name__ == "__main__":
             target = extract_target(srpm_infos, srpm)
             print "Building %s" % srpm
             cmd = ["rpmbuild", "--rebuild", "-v", "%s" % srpm,
-                   "--target", target, "--define", "_rpmdir %s" % tmp_path]
+                   "--target", target, "--define", "_rpmdir %s" % TMP_RPM_PATH]
             (rc, stdout, stderr) = doexec(cmd)
-            if rc==0:
+            if rc == 0:
                 print "Success"
-                rpms = glob.glob(os.path.join(tmp_path, target, "*"))
+                rpms = glob.glob(os.path.join(TMP_RPM_PATH, target, "*"))
                 (rc, stdout, stderr) = doexec(["rpm", "-i"] + rpms)
                 if rc != 0:
-                    print "Ignoring failure installing rpm: %s" % rpm
+                    print "Ignoring failure installing rpm batch: %s" % rpms
                     print stderr
-                dst_dir = os.path.join(rpmspath, target)
+                dst_dir = os.path.join(RPMS_DIR, target)
                 if not os.path.exists(dst_dir):
                     os.makedirs(dst_dir)
-                for rpm in rpms:
-                    shutil.copy(rpm, dst_dir)
-                    os.unlink(rpm)
+                for bin_rpm in rpms:
+                    shutil.copy(bin_rpm, dst_dir)
+                    os.unlink(bin_rpm)
             else:
                 print "Failed to build rpm from srpm: %s" % srpm
                 print "\nstdout\n======\n%s" % stdout
--- configure.py
+++ configure.py
@@ -4,12 +4,13 @@ import sys
 import os.path
 from string import maketrans
 from subprocess import call
-import demjson
 import urlparse
 import subprocess
 import re
 import glob
 import shutil
+
+import demjson
 
 CONFIG = "./conf.json"
 SOURCESDIR = "./SOURCES"
@@ -31,7 +32,8 @@ def parse_config(conf_dir):
     f.close()
     return demjson.decode(json)
 
-def fetch_url(url,override):
+
+def fetch_url(url, override):
     """ Fetch a url, renaming it to 'override' if it exists.
 
     Only fetches if it doesn't already exist.
@@ -51,23 +53,32 @@ def fetch_url(url,override):
             sys.exit(1)
         return 1
 
-def fetch_git_source(path):
-    """ Fetches an archive of a git repository.
 
-    Returns a version string, the tarball's prefix, and the pathname of the tarball.
+def fetch_git_source(path):
     """
-    description=subprocess.Popen(["git", "--git-dir=%s/.git" % path, "describe", "--tags"], stdout=subprocess.PIPE).communicate()[0].strip()
+    Fetches an archive of a git repository.
+
+    Returns a version string, the tarball's prefix, and the pathname of
+    the tarball.
+    """
+    description = subprocess.Popen(
+        ["git", "--git-dir=%s/.git" % path,
+         "describe", "--tags"],
+        stdout=subprocess.PIPE).communicate()[0].strip()
     p = re.compile("[^0-9]*")
     m = p.search(description)
     l = len(m.group())
-    version = description[l:].translate(maketrans('-','+'))
+    version = description[l:].translate(maketrans('-', '+'))
     final_name = path.split("/")[-1]
     prefix = final_name
-    call(["rm", "-f", "%s/%s.tar" % (SOURCESDIR,final_name)])
-    call(["rm", "-f", "%s/%s.tar.gz" % (SOURCESDIR,final_name)])
-    call(["git","--git-dir=%s/.git" % path, "archive","--prefix=%s/" % prefix, "--format=tar", "HEAD", "-o", "%s/%s.tar" % (SOURCESDIR,final_name)])
-    call(["gzip","%s/%s.tar" % (SOURCESDIR,final_name)])    
-    return (version,prefix,"%s.tar.gz" % final_name)
+    call(["rm", "-f", "%s/%s.tar" % (SOURCESDIR, final_name)])
+    call(["rm", "-f", "%s/%s.tar.gz" % (SOURCESDIR, final_name)])
+    call(["git", "--git-dir=%s/.git" % path, "archive",
+          "--prefix=%s/" % prefix, "--format=tar", "HEAD", "-o",
+          "%s/%s.tar" % (SOURCESDIR, final_name)])
+    call(["gzip", "%s/%s.tar" % (SOURCESDIR, final_name)])
+    return (version, prefix, "%s.tar.gz" % final_name)
+
 
 def prepare_srpm(pkg):
     global number_skipped, number_fetched
@@ -76,14 +87,15 @@ def prepare_srpm(pkg):
     sources = pkg['sources']
     process_spec = False
 
-    # If the config file mentions a git repo, we assume we're building a development
-    # RPM, whose spec file needs preprocessing (process_spec=True).
+    # If the config file mentions a git repo, we assume we're building
+    # a development RPM, whose spec file needs preprocessing
+    # (process_spec=True).
     for source in sources:
-        scheme=urlparse.urlparse(source['url'])[0]
-        if scheme=='git':
+        scheme = urlparse.urlparse(source['url'])[0]
+        if scheme == 'git':
             process_spec = True
             if len(sources) > 1:
-                print "Can't cope with more than one source for a git type package"
+                print "Can't cope with multiple source for a git type package"
                 sys.exit(1)
 
     spec_path = spec
@@ -96,32 +108,38 @@ def prepare_srpm(pkg):
         sys.exit(1)
 
     for source in sources:
-        (scheme,netloc,path,params,query,fragment) = urlparse.urlparse(source['url'])
+        (scheme, netloc, path, params, query, fragment) = urlparse.urlparse(
+            source['url'])
 
-        if (scheme=='file' or scheme=='http' or scheme=='https'):
+        if (scheme == 'file' or scheme == 'http' or scheme == 'https'):
             url, override = source['url'], source['override']
-            result = fetch_url(url,override)
+            result = fetch_url(url, override)
             number_fetched += result
             number_skipped += (1 - result)
-        if scheme=='git':
+        if scheme == 'git':
             print "Configuring package with spec file: %s" % spec
-            (version,prefix,source_tarball) = fetch_git_source(path)
-            spec_contents=subprocess.Popen(["sed","-e","s/@VERSION@/%s/g" % version,
-                                            "-e","s/@PREFIX@/%s/g" % prefix, 
-                                            "-e","s/@SOURCE@/%s/g" % source_tarball,
-                                            "%s" % spec_path],stdout=subprocess.PIPE).communicate()[0]
-            f = open(spec,"w")
+            (version, prefix, source_tarball) = fetch_git_source(path)
+            spec_contents = subprocess.Popen(
+                ["sed", "-e", "s/@VERSION@/%s/g" % version,
+                 "-e", "s/@PREFIX@/%s/g" % prefix,
+                 "-e", "s/@SOURCE@/%s/g" % source_tarball,
+                 "%s" % spec_path],
+                stdout=subprocess.PIPE).communicate()[0]
+            f = open(spec, "w")
             f.write(spec_contents)
             f.close()
 
+
 def build_srpm(pkg):
-    call(["rpmbuild", "-bs", "%s/%s" % (conf_dir, pkg['spec']), "--nodeps", "--define", "_topdir ."])
+    call(["rpmbuild", "-bs", "%s/%s" % (conf_dir, pkg['spec']),
+          "--nodeps", "--define", "_topdir ."])
+
 
 if __name__ == "__main__":
     if len(sys.argv) != 2:
         print "Usage:"
         sys.exit(1)
-    conf_dir = sys.argv[1];
+    conf_dir = sys.argv[1]
 
     for dir in [SOURCESDIR, SRPMSDIR]:
         if os.path.exists(dir):
@@ -142,4 +160,3 @@ if __name__ == "__main__":
 
     print "number of packages skipped: %d" % number_skipped
     print "number of packages fetched: %d" % number_fetched
-
