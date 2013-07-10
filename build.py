05b41d9bd8be123ed92b2e1d89ea5939d58ce0de
Si Beaumont <simon.beaumont@citrix.com>
2013-07-10 10:22:53 +0100

    Return error when nothing configured
    Signed-off-by: Si Beaumont <simon.beaumont@citrix.com>

--- build.py
+++ build.py
@@ -10,7 +10,7 @@ import shutil
 
 import demjson
 
-SRPMS_DIR = "SRPMS"
+SRPMS_DIR = "./SRPMS/"
 RPMS_DIR = "RPMS"
 BUILD_DIR = "BUILD"
 TMP_RPM_PATH = "/tmp/RPMS"
@@ -164,6 +164,11 @@ def write_rpmmacros():
     f.close()
 
 if __name__ == "__main__":
+    if not os.path.isdir(SRPMS_DIR) or not os.listdir(SRPMS_DIR):
+        print ("Error: No srpms found in %s; First run configure.py." %
+               SRPMS_DIR)
+        sys.exit(1)
+
     packages = glob.glob(os.path.join(SRPMS_DIR, '*.src.rpm'))
     write_rpmmacros()
     srpm_infos = map(get_srpm_info, packages)
