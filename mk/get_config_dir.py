51715af3249436ab28af1156ab0d59bdb6356446
Si Beaumont <simon.beaumont@citrix.com>
2013-10-18 12:55:22 +0100

    Fail if the COMPONENT is unknown when building
    Before this, we would try to chown -R the CONFIG_DIR + /* which was just / when
    the CONFIG_DIR was empty leading to an attempt to chown the entire file system.
    
    Signed-off-by: Si Beaumont <simon.beaumont@citrix.com>

--- mk/get_config_dir.py
+++ mk/get_config_dir.py
@@ -1,11 +1,14 @@
 #!/usr/bin/env python
 
+import sys
 import os
 
 component = os.getenv("COMPONENT")
 
 if component == "ocaml":
     print "/repos/xen-dist-ocaml.hg"
-if component == "api-libs":
+elif component == "api-libs":
     print "/repos/xen-api-libs-specs"
-
+else:
+    sys.stderr.write("Unknown component: '%s'\n" % component)
+    sys.exit(1)
