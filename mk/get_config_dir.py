8bee2901b1dd4919b9aa3c6523b11e7ec316aec9
Si Beaumont <simon.beaumont@citrix.com>
2013-10-18 12:57:14 +0100

    Add api-backports -> xen-api-backports to conifig
    Signed-off-by: Si Beaumont <simon.beaumont@citrix.com>

--- mk/get_config_dir.py
+++ mk/get_config_dir.py
@@ -9,6 +9,8 @@ if component == "ocaml":
     print "/repos/xen-dist-ocaml.hg"
 elif component == "api-libs":
     print "/repos/xen-api-libs-specs"
+elif component == "api-backports":
+    print "/repos/xen-api-backports"
 else:
     sys.stderr.write("Unknown component: '%s'\n" % component)
     sys.exit(1)
