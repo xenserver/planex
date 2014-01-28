04807105282211ff4ad79e8b4e9b13442e083c86
Euan Harris <euan.harris@citrix.com>
2014-01-28 12:10:42 +0000

    globals: Replace deprecated 'map' builtin with list comprehension
    Signed-off-by: Euan Harris <euan.harris@citrix.com>

--- planex_globals.py
+++ planex_globals.py
@@ -2,8 +2,8 @@ import os.path
 
 BUILD_ROOT_DIR = "planex-build-root"
 
-[SPECS_DIR, SOURCES_DIR, SRPMS_DIR, RPMS_DIR, BUILD_DIR] = map(
-    lambda x: os.path.join(BUILD_ROOT_DIR, x),
-    ['SPECS', 'SOURCES', 'SRPMS', 'RPMS', 'BUILD'])
+[SPECS_DIR, SOURCES_DIR, SRPMS_DIR, RPMS_DIR, BUILD_DIR] = [
+    os.path.join(BUILD_ROOT_DIR, dir_name) for dir_name in
+    ['SPECS', 'SOURCES', 'SRPMS', 'RPMS', 'BUILD']]
 
 SPECS_GLOB = os.path.join(SPECS_DIR, "*.spec")
