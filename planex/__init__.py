85cadc8034afa65860db39cb9e174b1b35c1f8a6
Euan Harris <euan.harris@citrix.com>
2014-02-03 13:40:54 +0000

    planex: Import xenserver-core's Spec wrapper class
    Signed-off-by: Euan Harris <euan.harris@citrix.com>

---
+++ planex/__init__.py
@@ -0,0 +1,1 @@
+"""Library of build and packaging utilities"""
--- setup.py
+++ setup.py
@@ -3,6 +3,7 @@ from distutils.core import setup
 setup(name='planex',
       version='0.0.0',
       py_modules=['planex_globals'],
+      packages=['planex'],
       scripts=['planex-configure', 'planex-build', 'planex-install',
                'spectool']
       )
