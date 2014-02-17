18210f59ef37676fdbfff0e85674a737e861e0f4
Mate Lakat <mate.lakat@citrix.com>
2014-02-17 15:50:53 +0000

    Fix setup.py

--- setup.py
+++ setup.py
@@ -4,11 +4,11 @@ setup(name='planex',
       version='0.0.0',
       py_modules=['planex_globals'],
       entry_points={
-          'console_scripts': {
+          'console_scripts': [
               'planex-configure = planex.configure:main',
               'planex-build = planex.build:main',
               'planex-install = planex.install:main',
-          }
+          ]
       },
       scripts=['bin/spectool']
       )
