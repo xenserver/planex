32316673837882f0e1c3d51f81d1fe6410af160f
Jon Ludlam <jonathan.ludlam@citrix.com>
2014-04-24 11:17:38 +0100

    Remove use of fs python library for ease of deployment

--- planex/sources.py
+++ planex/sources.py
@@ -1,38 +1,97 @@
+import os
 import re
 from planex import exceptions
 
-
-class GitHubSource(object):
+class SCM(object):
     def __init__(self, url):
-        match = re.match(
-            r'''
-            ^
-            (?P<repo_url>
-                git://github.com/
-                (?P<username>[^#^/]+)/
-                (?P<reponame>[^#^/]+)
-            )
-            (\#(?P<branch>.+))?
-            $
-            ''',
-            url, re.VERBOSE)
-        if not match:
-            raise exceptions.InvalidURL(url)
-        self.repo_url = match.group('repo_url')
-        self.branch = match.group('branch') or 'master'
-        self.username = match.group('username')
-        self.reponame = match.group('reponame')
+        ty = url.split(":")[0]
+        urlparts = url.split('#')
+        repo_url = urlparts[0]
+        repo_name = urlparts[0].split('/')[-1]
 
-    @property
-    def path(self):
-        return '/'.join([self.username, self.reponame + '.git'])
+        branch= None
+        if len(urlparts)>1:
+            branch = urlparts[1]
+        
+        if repo_name.endswith(".git"):
+            repo_name = repo_name[:-4]
 
-    def clone_commands(self, filesystem):
-        return [
+        self.ty        = ty
+        self.repo_url  = repo_url
+        self.repo_name = repo_name
+        self.branch    = branch
+ 
+    def clone_commands(self, path):
+        raise Exception
+
+class GitSource(SCM):
+    def __init__(self, url):
+        super(GitSource, self).__init__(url)
+
+    @staticmethod
+    def handles(ty):
+        return ty=="git"
+
+    def clone_commands(self, path):
+        if self.branch:
+            branchcmd = ["--branch",self.branch]
+        else:
+            branchcmd = []
+
+        cmd = [
             'git',
             'clone',
             self.repo_url,
-            '--branch',
-            self.branch,
-            filesystem.getsyspath(self.path)
+            os.path.join(path,self.repo_name)
+        ] + branchcmd
+
+        return cmd
+
+class HgSource(SCM):
+    def __init__(self, url):
+        super(HgSource, self).__init__(url)
+
+    @staticmethod
+    def handles(ty):
+        return ty=="hg"
+
+    def clone_commands(self, path):
+        if self.branch:
+            clone_url = self.repo_url + "#" + self.branch
+        else:
+            clone_url = self.repo_url
+
+        clone_url = re.sub('hg://', "http://", clone_url)
+
+        return [
+            'hg',
+            'clone',
+            clone_url,
+            os.path.join(path,self.repo_name)
         ]
+
+class FileSource(SCM):
+    def __init__(self, url):
+        super(FileSource, self).__init__(url)
+
+    @staticmethod
+    def handles(ty):
+        return ty in ["file", "http", "https"]
+
+    def clone_commands(self, path):
+        return []
+
+class OtherSource(SCM):
+    @staticmethod
+    def handles(ty):
+        return False
+
+    def clone_commands(self, path):
+        return []
+
+def Source(url):
+    ty = url.split(":")[0]
+    for cls in SCM.__subclasses__():
+        if cls.handles(ty):
+            return cls(url)
+    return OtherSource(url)
