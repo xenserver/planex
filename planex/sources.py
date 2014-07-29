ec421bebe30d5fb5b36ae23c44174ec1c34aef09
Jon Ludlam <jonathan.ludlam@citrix.com>
2014-07-29 18:17:06 +0100

    Add back in the ability to use a github mirror directory
    Also bring the test coverage of sources.py up to 100%!
    
    Signed-off-by: Jon Ludlam <jonathan.ludlam@citrix.com>

--- planex/sources.py
+++ planex/sources.py
@@ -7,7 +7,9 @@ from planex.globals import SOURCES_DIR
 from planex.util import run
 
 class SCM(object):
-    def __init__(self, url, repomirror):
+    repos = "repos"
+
+    def __init__(self, url, config):
         for protocol in ['git', 'hg']:
             if protocol not in urlparse.uses_fragment:
                 urlparse.uses_fragment.append(protocol)
@@ -16,8 +18,16 @@ class SCM(object):
         urlparts = url.split('#')
         repo_url = "%s://%s%s" % (scheme, host, path) # Strip of fragment
         self.repo_name = path.split('/')[-1]
-        self.repomirror = repomirror
 
+        def absolutize(path):
+            if os.path.isabs(path):
+                return path
+            else:
+                return os.path.join(os.getcwd(),path)
+
+        self.repos_mirror_path = absolutize(config.repos_mirror_path)
+        self.repos_path = absolutize(config.repos_path)
+        
         def strip_ext(ext):
             if self.repo_name.endswith(ext):
                 self.repo_name = self.repo_name[:-len(ext)]
@@ -39,7 +49,7 @@ class SCM(object):
 
     @property
     def localpath(self):
-        return os.path.join(self.repomirror, self.repo_name)
+        return os.path.join(self.repos_path, self.repo_name)
 
     @property
     def tarballprefix(self):
@@ -57,8 +67,8 @@ class SCM(object):
         
 
 class GitSource(SCM):
-    def __init__(self, url, repomirror):
-        super(GitSource, self).__init__(url, repomirror)
+    def __init__(self, url, config):
+        super(GitSource, self).__init__(url, config)
         if self.fragment:
             self.git_committish = self.fragment.split('/')[0]
         else:
@@ -80,38 +90,64 @@ class GitSource(SCM):
         # For a repo e.g. git://github.com/foo/bar.git
         # Check for a mirror of the form mirrorpath/github.com/foo/bar.git
         (_, host, urlpath, _, _, _) = urlparse.urlparse(self.repo_url)
-        clone_cmd = [
-            'git',
-            'clone',
-            self.repo_url,
-            dst
-        ]
-        checkout_cmd = [
-            'git',
-            '--git-dir=%s' % dst_dotgit,
-            '--work-tree=%s' % dst,
-            'checkout',
-            self.git_committish
-        ]
-        return [clone_cmd, checkout_cmd]
+        
+        mirrorpath = os.path.join(self.repos_mirror_path, host, urlpath.strip("/"))
 
-    def pin(self, scmhash=None):
+        if os.path.exists(mirrorpath):
+            clone_cmd = [
+                'git',
+                'clone',
+                mirrorpath,
+                dst
+            ]
+            reset_urls_cmd = [
+                'git',
+                '--git-dir=%s' % dst_dotgit,
+                'remote',
+                'set-url',
+                'origin',
+                self.repo_url
+            ]
+            fetch_cmd = [
+                'git',
+                '--git-dir=%s' % dst_dotgit,
+                'remote',
+                'fetch',
+                '--all',
+                '-t'
+            ]
+            checkout_cmd = [
+                'git',
+                '--git-dir=%s' % dst_dotgit,
+                '--work-tree=%s' % dst,
+                'checkout',
+                self.git_committish
+            ]
+            return [clone_cmd, reset_urls_cmd, fetch_cmd, checkout_cmd]
+        else:
+            clone_cmd = [
+                'git',
+                'clone',
+                self.repo_url,
+                dst
+            ]
+            checkout_cmd = [
+                'git',
+                '--git-dir=%s' % dst_dotgit,
+                '--work-tree=%s' % dst,
+                'checkout',
+                self.git_committish
+            ]
+            return [clone_cmd, checkout_cmd]
+
+    def pin(self):
         dotgitdir = os.path.join(self.localpath, ".git")
 
-        if scmhash == None:
-            # First, get the hash of the commit
-            cmd = ["git", "--git-dir=%s" % dotgitdir,
-                   "rev-parse", "HEAD"]
+        # First, get the hash of the commit
+        cmd = ["git", "--git-dir=%s" % dotgitdir,
+               "rev-parse", "HEAD"]
 
-            scmhash = run(cmd)['stdout'].strip()
-        else:
-            cmd = ["git", "--git-dir=%s" % dotgitdir,
-                   "show", scmhash]
-            try:
-                run(cmd)
-            except:
-                print "Invalid hash '%s' for repo '%s'" % (scmhash, self.localpath)
-                raise
+        scmhash = run(cmd)['stdout'].strip()
 
         # Verified good hash.
         self.scmhash = scmhash
@@ -141,23 +177,20 @@ class GitSource(SCM):
         if os.path.exists(os.path.join(sources_dir, self.archivename)):
             return
         
-        if self.archivename.endswith(".gz"):
-            tarball_name = self.archivename[:-3]
-        else:
-            tarball_name = self.archivename
+        # archive name always ends in .gz - strip it off
+        tarball_name = self.archivename[:-3]
 
         cmd = ["git", "--git-dir=%s" % dotgitdir, "archive",
                "--prefix=%s/" % self.tarballprefix, self.scmhash, "-o",
                "%s/%s" % (sources_dir, tarball_name)]
         run(cmd)
 
-        if self.archivename.endswith(".gz"):
-            cmd = ["gzip", "-f", "%s/%s" % (sources_dir, tarball_name)]
-            run(cmd)
+        cmd = ["gzip", "-f", "%s/%s" % (sources_dir, tarball_name)]
+        run(cmd)
 
 class HgSource(SCM):
-    def __init__(self, url, repomirror):
-        super(HgSource, self).__init__(url, repomirror)
+    def __init__(self, url, config):
+        super(HgSource, self).__init__(url, config)
         if os.path.exists(self.localpath):
             # Don't pin if the repo doesn't currently exist
             self.pin()
@@ -169,7 +202,7 @@ class HgSource(SCM):
     @property
     def localpath(self):
         # Mercurial repos clone with the extension .hg
-        return os.path.join(self.repomirror, "%s.hg" % self.repo_name)
+        return os.path.join(self.repos_path, "%s.hg" % self.repo_name)
 
     def clone_commands(self):
         dst = self.localpath
@@ -188,18 +221,10 @@ class HgSource(SCM):
             dst
         ]]
 
-    def pin(self, scmhash=None):
-        if scmhash == None:
-            cmd = ["hg", "-R", self.localpath, "tip", "--template", "{node}"]
-            scmhash = run(cmd)['stdout'].strip()
-        else:
-            cmd = ["hg", "-R", self.localpath, "export", scmhash]
-            try:
-                run(cmd)
-            except:
-                print "Invalid hash '%s' for repo '%s'" % (scmhash, self.localpath)
-                raise
+    def pin(self):
 
+        cmd = ["hg", "-R", self.localpath, "tip", "--template", "{node}"]
+        scmhash = run(cmd)['stdout'].strip()
         self.scmhash = scmhash
 
         cmd = ["hg", "-R", self.localpath, "parents", "--template", "{rev}"]
@@ -210,8 +235,10 @@ class HgSource(SCM):
     def archive(self, sources_dir=SOURCES_DIR):    
         # If it already exists, we're done.
         if os.path.exists(os.path.join(sources_dir, self.archivename)):
+            print "File's already here!"
             return
         
+        print "File's not here!"
         cmd = ["hg", "-R", self.localpath, "archive", "-t", "tgz", "-p", 
                "%s/" % self.tarballprefix, 
                "%s/%s" % (sources_dir, self.archivename)]
@@ -220,8 +247,8 @@ class HgSource(SCM):
 
 
 class FileSource(SCM):
-    def __init__(self, url, repomirror):
-        super(FileSource, self).__init__(url, repomirror)
+    def __init__(self, url, config):
+        super(FileSource, self).__init__(url, config)
 
     @property
     def archivename(self):
@@ -235,7 +262,7 @@ class FileSource(SCM):
         return []
 
     def archive(self, sources_dir=SOURCES_DIR):
-        final_path = os.path.join(SOURCES_DIR, self.archivename)
+        final_path = os.path.join(sources_dir, self.archivename)
         if os.path.exists(final_path):
             return
         run(["curl", "-k", "-L", "-o", final_path, self.orig_url])
