3fb1b28215e0c26685ba271d262550ff9f1fa376
Euan Harris <euan.harris@citrix.com>
2015-01-05 17:50:42 +0000

    depend: Move source file download rule into static Makefile
    Signed-off-by: Euan Harris <euan.harris@citrix.com>

--- planex/depend.py
+++ planex/depend.py
@@ -49,8 +49,6 @@ def download_rpm_sources(spec, args):
         # Source comes from a remote HTTP server
         if source.scheme in ["http", "https"]:
             print '%s: %s' % (path, spec.specpath())
-            print '\t@echo [DOWNLOADER] $@'
-            print '\t@planex-downloader %s %s' % (url, path)
 
         # Source comes from a local file or directory
         if source.scheme == "file":
