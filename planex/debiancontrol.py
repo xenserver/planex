ba386f7b1c171a2336016cf385478c62b3052907
Euan Harris <euan.harris@citrix.com>
2015-02-04 15:54:48 +0000

    debiancontrol: Fix PEP8 warnings
    Signed-off-by: Euan Harris <euan.harris@citrix.com>

--- planex/debiancontrol.py
+++ planex/debiancontrol.py
@@ -16,15 +16,17 @@ def control_from_spec(spec):
 
 def source_deb_from_spec(spec, tree):
     res = ""
-    res += "Source: %s\n" % mappkgname.map_package(spec.sourceHeader['name'])[0]
+    res += "Source: %s\n" % \
+        mappkgname.map_package(spec.sourceHeader['name'])[0]
     res += "Priority: %s\n" % "optional"
     res += "Maintainer: %s\n" % "Euan Harris <euan.harris@citrix.com>"
     res += "Section: %s\n" % mappkgname.map_section(spec.sourceHeader['group'])
     res += "Standards-Version: %s\n" % STANDARDS_VERSION
 
     res += "Build-Depends:\n"
-    build_depends = ["debhelper (>= 8)", "dh-ocaml (>= 0.9)", "ocaml-nox", "python"]
-    for pkg, version in zip(spec.sourceHeader['requires'], 
+    build_depends = ["debhelper (>= 8)", "dh-ocaml (>= 0.9)", "ocaml-nox",
+                     "python"]
+    for pkg, version in zip(spec.sourceHeader['requires'],
                             spec.sourceHeader['requireVersion']):
         deps = mappkgname.map_package(pkg)
         for dep in deps:
@@ -48,7 +50,7 @@ def binary_deb_from_spec(spec, tree):
 
     res += "Depends:\n"
     depends = ["${ocaml:Depends}", "${shlibs:Depends}", "${misc:Depends}"]
-    for pkg, version in zip(spec.header['requires'], 
+    for pkg, version in zip(spec.header['requires'],
                             spec.header['requireVersion']):
         deps = mappkgname.map_package(pkg)
         for dep in deps:
@@ -74,8 +76,7 @@ def format_description(description):
     and blank lines must be replaced by dots"""
 
     paragraphs = "".join(description).split("\n\n")
-    wrapped = ["\n".join(textwrap.wrap(p, initial_indent=" ", 
-                                       subsequent_indent=" ")) 
-                for p in paragraphs]
+    wrapped = ["\n".join(textwrap.wrap(p, initial_indent=" ",
+                                       subsequent_indent=" "))
+               for p in paragraphs]
     return "\n .\n".join(wrapped)
-
