ee72007af923e3eba3821859572125e9a608137f
Euan Harris <euan.harris@citrix.com>
2015-02-04 09:40:43 +0000

    clone: Fix pylint warnings
    Signed-off-by: Euan Harris <euan.harris@citrix.com>

--- planex/clone.py
+++ planex/clone.py
@@ -1,3 +1,7 @@
+"""
+planex-clone: Download sources required by spec files
+"""
+
 import argparse
 import sys
 import logging
@@ -7,16 +11,18 @@ import os
 
 from planex import sources
 from planex import executors
-from planex import util
 
 
 log = logging.getLogger(__name__)
 
 
 def parse_args_or_exit(argv=None):
+    """
+    Parse command line options
+    """
     parser = argparse.ArgumentParser()
-    parser.add_argument('--config_dir', help='Configuration directory',
-	default=".")
+    parser.add_argument(
+        '--config_dir', help='Configuration directory', default=".")
     parser.add_argument(
         '--print-only', help='Only print sources, do not clone them',
         action='store_true')
@@ -27,29 +33,36 @@ def parse_args_or_exit(argv=None):
         '--quiet', help='Do not print warnings',
         action='store_true')
     parser.add_argument(
-        '--repos_mirror_path', help='Path to a local repository mirror directory. '
-        'This should be a file path where for a git url '
-        '"git://host.com/some/path.git" the mirror '
-        'should contain <mirror_path>/host.com/some/path.git', 
+        '--repos_mirror_path',
+        help='Path to a local repository mirror directory. '
+             'This should be a file path where for a git url '
+             '"git://host.com/some/path.git" the mirror '
+             'should contain <mirror_path>/host.com/some/path.git',
         default="")
     parser.add_argument(
-        '--repos_path', help='Local path under which the repositories should be '
-        'checked out',
+        '--repos_path',
+        help='Local path under which the repositories should be checked out',
         default="repos")
     parser.add_argument(
-        '--specs_path', help='Path (relative to config_dir) to the SPECS directory containing spec '
-        'files to be preprocessed as well as those simply to be built.',
+        '--specs_path',
+        help='Path (relative to config_dir) to the SPECS directory '
+             'containing spec files to be preprocessed as well as those '
+             'simply to be built.',
         default="SPECS")
     return parser.parse_args(argv)
 
 
 def main():
+    """
+    Entry point
+    """
     config = parse_args_or_exit()
 
     logging.basicConfig(level=logging.ERROR if config.quiet else logging.DEBUG)
 
-    templates = [planex.spec.Spec(path) 
-                 for path in glob.glob(os.path.join(config.config_dir, config.specs_path, "*.spec.in"))]
+    specs_path = os.path.join(config.config_dir,
+                              config.specs_path, "*.spec.in")
+    templates = [planex.spec.Spec(path) for path in glob.glob(specs_path)]
 
     if config.print_only:
         for template in templates:
@@ -62,12 +75,13 @@ def main():
         executor = executors.RealExecutor()
 
     for template in templates:
-        srcs = [sources.Source(url,config) for url in template.source_urls()]
+        srcs = [sources.Source(url, config) for url in template.source_urls()]
 
         commands_list = [src.clone_commands() for src in srcs]
 
         log.info(commands_list)
-        results_list = [[executor.run(command) for command in commands] for commands in commands_list]
+        results_list = [[executor.run(command) for command in commands]
+                        for commands in commands_list]
 
         for results in results_list:
             for result in results:
