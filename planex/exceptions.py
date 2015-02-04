50a03f2ac8ea67ca991817a95ea6a1ef44e513f1
Euan Harris <euan.harris@citrix.com>
2015-02-04 16:03:51 +0000

    exceptions: Fix pylint and PEP8 warnings
    Signed-off-by: Euan Harris <euan.harris@citrix.com>

--- planex/exceptions.py
+++ planex/exceptions.py
@@ -1,11 +1,18 @@
+"""
+General execeptions used in planex
+"""
+
+
 class NoSuchFile(Exception):
+    """File does not exist"""
     pass
 
 
 class InvalidURL(Exception):
+    """URL is invalid"""
     pass
 
 
 class NoRepository(Exception):
+    """No repository exists at path"""
     pass
-
