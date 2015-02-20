d26536096e2da2d26c4238124c84bd06a8ab4774
Euan Harris <euan.harris@citrix.com>
2015-02-20 14:02:05 +0000

    tests: Get depend tests running again

--- tests/test_depend.py
+++ tests/test_depend.py
@@ -7,70 +7,62 @@ import os
 import sys
 import unittest
 
-import depend
-import pkg
+import planex.spec
+import planex.depend
 
 
 class BasicTests(unittest.TestCase):
     def setUp(self):
-        self.spec = pkg.Spec("SPECS/ocaml-cohttp.spec", dist=".el6")
+        self.spec = planex.spec.Spec("tests/data/ocaml-cohttp.spec",
+                                     dist=".el6", topdir=".")
 
     def test_build_srpm_from_spec(self):
-        depend.build_srpm_from_spec(self.spec)
+        planex.depend.build_srpm_from_spec(self.spec)
 
         self.assertEqual(
             sys.stdout.getvalue(),
             "./SRPMS/ocaml-cohttp-0.9.8-1.el6.src.rpm: "
             "./SPECS/ocaml-cohttp.spec "
-            "./SOURCES/ocaml-cohttp-0.9.8.tar.gz\n")
+            "./SOURCES/ocaml-cohttp-0.9.8.tar.gz "
+            "./SOURCES/ocaml-cohttp-extra-0.9.8.tar.gz "
+            "./SOURCES/ocaml-cohttp-init\n")
 
     def test_download_rpm_sources(self):
-        depend.download_rpm_sources(self.spec)
+        planex.depend.download_rpm_sources(self.spec, None)
 
         self.assertEqual(
             sys.stdout.getvalue(),
             "./SOURCES/ocaml-cohttp-0.9.8.tar.gz: ./SPECS/ocaml-cohttp.spec\n"
-            "	@echo [CURL] $@\n"
-            "	@curl --silent --show-error -L -o $@ https://github.com/"
-            "mirage/ocaml-cohttp/archive/ocaml-cohttp-0.9.8/"
-            "ocaml-cohttp-0.9.8.tar.gz\n")
+            "./SOURCES/ocaml-cohttp-extra-0.9.8.tar.gz: "
+            "./SPECS/ocaml-cohttp.spec "
+            "$(shell find /code/ocaml-cohttp-extra)\n"
+            "	@echo [GIT] $@\n"
+            "	@git --git-dir=/code/ocaml-cohttp-extra/.git archive "
+            "--prefix ocaml-cohttp-extra-0.9.8/ -o $@ HEAD\n")
 
     def test_build_rpm_from_srpm(self):
-        depend.build_rpm_from_srpm(self.spec)
+        planex.depend.build_rpm_from_srpm(self.spec)
 
         self.assertEqual(
             sys.stdout.getvalue(),
             "./RPMS/x86_64/ocaml-cohttp-0.9.8-1.el6.x86_64.rpm: "
-            "./SRPMS/ocaml-cohttp-0.9.8-1.el6.src.rpm\n"
-            "./RPMS/x86_64/ocaml-cohttp-devel-0.9.8-1.el6.x86_64.rpm: "
             "./SRPMS/ocaml-cohttp-0.9.8-1.el6.src.rpm\n")
 
     def test_buildrequires_for_rpm(self):
-        spec_paths = glob.glob(os.path.join("./SPECS", "*.spec"))
-        specs = [pkg.Spec(spec_path, dist='.el6') for spec_path in spec_paths]
+        # N.B. buildrequires_for_rpm only generates rules for other packages
+        # defined by local spec files.   Even though ocaml-cohttp depends on
+        # other packages, the test data directory contains only ocaml-uri and
+        # ocaml-cstruct.
+        spec_paths = glob.glob(os.path.join("tests/data", "ocaml-*.spec"))
+        specs = [planex.spec.Spec(spec_path, dist='.el6')
+                 for spec_path in spec_paths]
 
-        depend.buildrequires_for_rpm(self.spec,
-                                     depend.package_to_rpm_map(specs))
+        planex.depend.buildrequires_for_rpm(
+            self.spec, planex.depend.package_to_rpm_map(specs))
 
         self.assertEqual(
             sys.stdout.getvalue(),
             "./RPMS/x86_64/ocaml-cohttp-0.9.8-1.el6.x86_64.rpm: "
-            "./RPMS/x86_64/ocaml-uri-devel-1.3.8-1.el6.x86_64.rpm\n"
+            "./RPMS/x86_64/ocaml-uri-1.6.0-1.el6.x86_64.rpm\n"
             "./RPMS/x86_64/ocaml-cohttp-0.9.8-1.el6.x86_64.rpm: "
-            "./RPMS/x86_64/ocaml-cstruct-devel-0.7.1-2.el6.x86_64.rpm\n"
-            "./RPMS/x86_64/ocaml-cohttp-0.9.8-1.el6.x86_64.rpm: "
-            "./RPMS/x86_64/ocaml-ounit-devel-1.1.2-3.el6.x86_64.rpm\n"
-            "./RPMS/x86_64/ocaml-cohttp-0.9.8-1.el6.x86_64.rpm: "
-            "./RPMS/x86_64/ocaml-re-devel-1.2.1-1.el6.x86_64.rpm\n"
-            "./RPMS/x86_64/ocaml-cohttp-0.9.8-1.el6.x86_64.rpm: "
-            "./RPMS/x86_64/ocaml-lwt-devel-2.4.3-1.el6.x86_64.rpm\n"
-            "./RPMS/x86_64/ocaml-cohttp-devel-0.9.8-1.el6.x86_64.rpm: "
-            "./RPMS/x86_64/ocaml-uri-devel-1.3.8-1.el6.x86_64.rpm\n"
-            "./RPMS/x86_64/ocaml-cohttp-devel-0.9.8-1.el6.x86_64.rpm: "
-            "./RPMS/x86_64/ocaml-cstruct-devel-0.7.1-2.el6.x86_64.rpm\n"
-            "./RPMS/x86_64/ocaml-cohttp-devel-0.9.8-1.el6.x86_64.rpm: "
-            "./RPMS/x86_64/ocaml-ounit-devel-1.1.2-3.el6.x86_64.rpm\n"
-            "./RPMS/x86_64/ocaml-cohttp-devel-0.9.8-1.el6.x86_64.rpm: "
-            "./RPMS/x86_64/ocaml-re-devel-1.2.1-1.el6.x86_64.rpm\n"
-            "./RPMS/x86_64/ocaml-cohttp-devel-0.9.8-1.el6.x86_64.rpm: "
-            "./RPMS/x86_64/ocaml-lwt-devel-2.4.3-1.el6.x86_64.rpm\n")
+            "./RPMS/x86_64/ocaml-cstruct-1.4.0-1.el6.x86_64.rpm\n")
