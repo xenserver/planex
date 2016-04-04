c7bea24ea3d8d8877ca54e0a5cb17773e09c15a8
Salvatore Cambria <salvatore.cambria@citrix.com>
2016-04-04 16:50:38 +0100

    make some improvement to makesrpm.py

--- planex/makesrpm.py
+++ planex/makesrpm.py
@@ -27,7 +27,7 @@ def parse_args_or_exit(argv):
         "--topdir", metavar="DIR", default=None,
         help='Set rpmbuild toplevel directory')
     parser.add_argument(
-        "--dist", metavar="DIST", default="",
+        "--dist", metavar="DIST", default=None,
         help="distribution tag (used in RPM filenames)")
     argcomplete.autocomplete(parser)
     return parser.parse_known_args(argv)
@@ -38,22 +38,23 @@ def parse_patchseries(series_file):
     Parse series file and return the list of patches
     """
     patches = []
-    for line in open(series_file):
-        line = line.strip()
-        if not line.startswith("#"):
-            patches.append(line)
+    with open(series_file) as series:
+        for line in series:
+            line = line.partition('#')[0].strip()
+            if line:
+                patches.append(line)
 
     return patches
 
 
 def setup_tmp_area():
     """
-    Create temporary wotking area
+    Create temporary working area
     """
-    tmp_dirpath = tempfile.mkdtemp() + '/'
-    tmp_specs = tmp_dirpath + 'SPECS/'
-    tmp_build = tmp_dirpath + '_build/'
-    tmp_sources = tmp_build + 'SOURCES/'
+    tmp_dirpath = tempfile.mkdtemp()
+    tmp_specs = os.path.join(tmp_dirpath, 'SPECS')
+    tmp_build = os.path.join(tmp_dirpath, '_build')
+    tmp_sources = os.path.join(tmp_build, 'SOURCES')
 
     os.makedirs(tmp_specs)
     os.makedirs(tmp_build)
@@ -62,72 +63,83 @@ def setup_tmp_area():
     return (tmp_dirpath, tmp_sources)
 
 
+def extract_patches(tmp_specfile, patchqueue_filters, patchqueue_path,
+                    tmp_sources, target):
+    """
+    Extract patches and inject them into the specfile
+    """
+    for line in fileinput.input(tmp_specfile, inplace=True):
+        if any([ext in line for ext in patchqueue_filters]):
+            tar = tarfile.open(patchqueue_path)
+            for mem in tar.getmembers():
+                # Modify mem.name in place to change the name of the
+                # extracted file (and drop any leading paths)
+                mem.name = '%s-' % target + os.path.basename(mem.name)
+                tar.extract(mem, tmp_sources)
+            patches = parse_patchseries(os.path.join(tmp_sources,
+                                                     '%s-series' % target))
+            print "# Patches for %s" % target
+            for patch_num, patch in enumerate(patches):
+                if patch:
+                    print "Patch%s: %%{name}-%s" % (patch_num, patch)
+        else:
+            print line,
+
+
+def get_command_line(intercepted_args, tmp_sources, tmp_specfile):
+    """
+    Return rpmbuild command line and arguments
+    """
+    cmd = ['rpmbuild']
+    if intercepted_args.quiet:
+        cmd.append('--quiet')
+    if intercepted_args.topdir is not None:
+        cmd.append('--define')
+        cmd.append('_topdir %s' % intercepted_args.topdir)
+    if intercepted_args.dist is not None:
+        cmd.append('--define')
+        cmd.append('%%dist %s' % intercepted_args.dist)
+    cmd.append('--define')
+    cmd.append('_sourcedir %s' % tmp_sources)
+    cmd.append('-bs')
+    cmd.append(tmp_specfile)
+
+    return cmd
+
+
 def main(argv):
     """
     Entry point arguments: component.spec Source0 Source1 ... Patch0 Patch1 ...
     The specfile must always be present and it has to be the first in the list
     """
     intercepted_args, passthrough_args = parse_args_or_exit(argv)
-    target = os.path.basename(passthrough_args[0]).split('.')[0]
+    target = os.path.splitext(os.path.basename(passthrough_args[0]))[0]
     specfile = passthrough_args[0]
     tmp_dirpath, tmp_sources = setup_tmp_area()
-    tmp_specfile = tmp_dirpath + specfile
+    tmp_specfile = os.path.join(tmp_dirpath, specfile)
 
     try:
         # Copy files to temporary working area
         copyfile(specfile, tmp_specfile)
-        patchqueue = False
-        patchqueue_path = None
         patchqueue_filters = ['.pg.', '.pq.']
 
         for source in passthrough_args[1:]:
-            copyfile(source, tmp_dirpath + source)
             if any([ext in source for ext in patchqueue_filters]):
-                patchqueue = True
-                patchqueue_path = source
+                extract_patches(tmp_specfile, patchqueue_filters,
+                                source, tmp_sources, target)
+            else:
+                copyfile(source, os.path.join(tmp_dirpath, source))
 
-        if patchqueue:
-            # Extract patches
-            patch_num = 0
-            for line in fileinput.input(tmp_specfile, inplace=True):
-                if any([ext in line for ext in patchqueue_filters]):
-                    tar = tarfile.open(tmp_dirpath + patchqueue_path)
-                    for mem in tar.getmembers():
-                        mem.name = '%s-' % target + os.path.basename(mem.name)
-                        tar.extract(mem, tmp_sources)
-                    patches = parse_patchseries(tmp_sources +
-                                                '%s-series' % target)
-                    print "# Patches for %s" % target
-                    for patch in patches:
-                        if patch != "":
-                            print "Patch%s: %%{name}-%s" % (patch_num, patch)
-                            patch_num += 1
-                else:
-                    print line,
-
-        args = []
-        if intercepted_args.quiet:
-            args.append('--quiet')
-        if intercepted_args.topdir is not None:
-            args.append('--define')
-            args.append('_topdir %s' % intercepted_args.topdir)
-        if intercepted_args.dist != "":
-            args.append('--define')
-            args.append(u'%dist ' + intercepted_args.dist)
-        args.append('--define')
-        args.append('_sourcedir %s' % tmp_sources)
-        args.append('-bs')
-        args.append(tmp_specfile)
-        cmd = ['rpmbuild']
-        cmd.extend(args)
-
+        cmd = get_command_line(intercepted_args, tmp_sources, tmp_specfile)
         return_value = subprocess.call(cmd)
         sys.exit(return_value)
 
-    except Exception as inst:
-        print type(inst)
-        print inst.args
-        print inst
+    except IOError as exc:
+        print "Copyfile: destination location must be writable"
+        print "Exception: %s" % exc
+    except (tarfile.TarError, tarfile.ReadError) as exc:
+        print "Error when extracting patchqueue from tarfile"
+        print "Exception: %s" % exc
 
     finally:
         # Clean temporary area
