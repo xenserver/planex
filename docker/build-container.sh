fec07b169c87687486f19ae41bda1901fc5f7de1
Euan Harris <euan.harris@citrix.com>
2016-05-10 11:03:26 +0100

    docker: Add Dockerfiles for unstable and master
    Signed-off-by: Euan Harris <euan.harris@citrix.com>

--- docker/build-container.sh
+++ docker/build-container.sh
@@ -1,3 +1,5 @@
 #!/bin/sh -xe
 
 docker build -t planex-release:0.7.3 --force-rm=true -f Dockerfile.release .
+docker build -t planex-master --force-rm=true -f Dockerfile.master .
+docker build -t planex-unstable --force-rm=true -f Dockerfile.unstable .
