1041a8d08f4a3a5e92ea039c7d995b7faac6e286
Euan Harris <euan.harris@citrix.com>
2016-06-02 17:26:47 +0100

    packaging: Do not install Makefile.rules to /usr/share/planex (#275)
    Installing files to absolute paths in setup.py breaks virtualenv.
    Instead, install Makefile.rules alongside the rest of planex and
    add an option to planex-init which returns its path.
    
    Signed-off-by: Euan Harris <euan.harris@citrix.com>

--- setup.py
+++ setup.py
@@ -9,10 +9,9 @@ setup(name='planex',
       packages=['planex'],
       include_package_data=True,
       package_data={'planex': ['Makefile.rules']},
-      data_files=[('/usr/share/planex', ['planex/Makefile.rules'])],
       entry_points={
           'console_scripts': [
-              'planex-init = planex.init:main',
+              'planex-init = planex.init:_main',
               'planex-cache = planex.cache:_main',
               'planex-fetch = planex.fetch:_main',
               'planex-pin = planex.pin:_main',
