88ddf51629ca2caa3d7c2fda0f5b3f8f5d98ba6c
Salvatore Cambria <salvatore.cambria@citrix.com>
2016-08-17 17:01:53 +0100

    Call tar.getnames() only one time

--- planex/extract.py
+++ planex/extract.py
@@ -98,8 +98,9 @@ def archive_root(tar):
     """
     Return the name of the top level directory of the tarball
     """
-    topname = os.path.commonprefix(tar.getnames())
-    if topname in tar.getnames():
+    names = tar.getnames()
+    topname = os.path.commonprefix(names)
+    if topname in names:
         top_element = tar.getmember(topname)
         if top_element.isdir():
             return topname
--- planex/makesrpm.py
+++ planex/makesrpm.py
@@ -59,8 +59,9 @@ def extract_topdir(tmp_specfile, source):
     for line in fileinput.input(tmp_specfile, inplace=True):
         if 'autosetup' in line:
             tar = tarfile.open(source)
-            topname = os.path.commonprefix(tar.getnames())
-            if topname in tar.getnames():
+            names = tar.getnames()
+            topname = os.path.commonprefix(names)
+            if topname in names:
                 top_element = tar.getmember(topname)
                 if top_element.isdir():
                     print "%s -n %s" % (line.strip(), topname)
