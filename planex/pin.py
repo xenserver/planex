d8e12503dc840e82aed1ace8fcb4fb54894ed0cc
Euan Harris <euan.harris@citrix.com>
2016-09-13 13:20:43 +0100

    pin: Extract Git utility functions into a library module
    Signed-off-by: Euan Harris <euan.harris@citrix.com>

--- planex/pin.py
+++ planex/pin.py
@@ -16,77 +16,10 @@ import tempfile
 import argcomplete
 import rpm
 
+from planex import git
 from planex.util import add_common_parser_options
-from planex.util import run
 from planex.util import setup_logging
 from planex.util import setup_sigint_handler
-
-
-def dotgitdir_of_path(repo):
-    """
-    Returns the path to the dotgitdir of the repository.
-
-    Possible paths: <repo>/.git, <repo> or <repo>.git
-
-    """
-
-    # We often have bare repos checked out, e.g. /path/to/xen-api.git,
-    # which doesn't contain a '.git' dir inside. We want to support
-    # specifying this by only by providing the path to the repo, or by
-    # just specifying 'xen-api' and having this function check for
-    # 'xen-api.git'
-    possibilities = [os.path.join(repo, ".git"),
-                     repo,
-                     repo + ".git"]
-    matches = [x for x in possibilities if
-               os.path.exists(os.path.join(x, "HEAD"))]
-    if len(matches) == 1:
-        return matches[0]
-    else:
-        raise Exception("Pin target is not a git repository: '%s'" % repo)
-
-
-def describe(repo, treeish="HEAD"):
-    """
-    Return an RPM compatible version string for a git repo at a given commit
-    """
-    dotgitdir = dotgitdir_of_path(repo)
-
-    # First, get the hash of the commit
-    cmd = ["git", "--git-dir=%s" % dotgitdir, "rev-parse", treeish]
-    sha = run(cmd)['stdout'].strip()
-
-    # Now lets describe that hash
-    cmd = ["git", "--git-dir=%s" % dotgitdir, "describe", "--tags", sha]
-    description = run(cmd, check=False)['stdout'].strip()
-
-    # if there are no tags, use the number of commits
-    if description == "":
-        cmd = ["git", "--git-dir=%s" % dotgitdir, "log", "--oneline", sha]
-        commits = run(cmd)['stdout'].strip()
-        description = str(len(commits.splitlines()))
-
-    # replace '-' with '+' in description to not confuse rpm
-    match = re.search("[^0-9]*", description)
-    matchlen = len(match.group())
-    return description[matchlen:].replace('-', '+')
-
-
-def archive(repo, commit_hash, prefix, target_dir):
-    """
-    Archive a git repo at a given commit with a specified version prefix.
-    Returns the path to a tar.gz to be used as a source for building an RPM.
-    """
-    dotgitdir = dotgitdir_of_path(repo)
-
-    prefix = "%s-%s" % (os.path.basename(repo), prefix)
-    path = os.path.join(target_dir, "%s.tar" % prefix)
-
-    run(["git", "--git-dir=%s" % dotgitdir, "archive", commit_hash,
-         "--prefix=%s/" % prefix, "-o", path])
-    run(["gzip", "--no-name", "-f", path])
-
-    return path + ".gz"
 
 
 def pinned_spec_of_spec(spec_path, src_map):
@@ -181,12 +114,12 @@ def update(args):
         for (src_num, pin_target) in pinned_sources.iteritems():
             # we're assuming for now that the target is a git repository
             repo, _, treeish = pin_target.partition('#')
-            src_version = describe(repo, treeish if treeish else None)
+            src_version = git.describe(repo, treeish if treeish else None)
             logging.debug("Source%s pin target is at version %s",
                           src_num, src_version)
 
             tmpdir = tempfile.mkdtemp(prefix='planex-pin')
-            tmp_archive = archive(repo, treeish, orig_version, tmpdir)
+            tmp_archive = git.archive(repo, treeish, orig_version, tmpdir)
             tar_name = os.path.basename(tmp_archive).replace(orig_version,
                                                              src_version)
             tar_path = os.path.join(args.pins_dir, tar_name)
