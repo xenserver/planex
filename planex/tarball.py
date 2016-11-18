f786593ec986c42b9037f58483904a17238cbb9e
Euan Harris <euan.harris@citrix.com>
2016-11-18 13:09:32 +0000

    tarball: Add extract_dir() method
    Signed-off-by: Euan Harris <euan.harris@citrix.com>

--- planex/tarball.py
+++ planex/tarball.py
@@ -36,9 +36,9 @@ class Tarball(object):
         source_path = os.path.join(self.archive_root, self.prefix)
         res = []
         for mem in self.tarfile.getmembers():
-            if mem.isfile() and os.path.normpath(mem.path).startswith(source_path):
-               mem.path = os.path.relpath(mem.path, source_path)
-               res.append(mem)
+            if mem.isfile() and mem.path.startswith(source_path):
+                mem.path = os.path.relpath(mem.path, source_path)
+                res.append(mem)
         return res
 
     def extractfile(self, source):
@@ -56,11 +56,25 @@ class Tarball(object):
         """
         # Get the TarInfo object representing the file and re-set its
         # name.   Otherwise the file will be written to its full path.
-        source_path = os.path.join(self.archive_root, self.prefix, source)
-        mem = self.tarfile.getmember(source_path)
-        mem.name = os.path.basename(source_path)
+        if isinstance(source, basestring):
+            source_path = os.path.join(self.archive_root, self.prefix, source)
+            mem = self.tarfile.getmember(source_path)
+        else:
+            mem = source
+        mem.name = os.path.basename(mem.name)
         self.tarfile.extract(mem, destdir)
         os.utime(os.path.join(destdir, mem.name), None)
+
+    def extract_dir(self, sourcedir, destdir):
+        """
+        Extract a whole directory from the tarball, saving it to destdir.
+        """
+        # Get the TarInfo object representing the file and re-set its
+        # name.   Otherwise the file will be written to its full path.
+        members = self.getmembers()
+        for mem in members:
+            if mem.name.startswith(sourcedir):
+                self.extract(mem, destdir)
 
 
 def archive_root(tar):
