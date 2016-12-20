7f9ce8eb3ac50a58bdbcd313c95328ea92c8ef9b
Marcello Seri <marcello.seri@citrix.com>
2016-12-20 10:28:56 +0000

    override: improve cli ergonomics
    The pinsdir, commitish and base repository are now customisable.
    
    Override will look for the presence of link or spec files and add the
    correct fields also when using linked packages.
    
    Signed-off-by: Marcello Seri <marcello.seri@citrix.com>

--- planex/cmd/override.py
+++ planex/cmd/override.py
@@ -14,6 +14,19 @@ from planex.git import current_branch
 from planex.util import add_common_parser_options
 
 
+def mkdir_p(path):
+    """
+    Like `mkdir -p path` but does not fail when the path is already present.
+    """
+    try:
+        os.makedirs(path)
+    except OSError as exc:
+        if exc.errno == errno.EEXIST and os.path.isdir(path):
+            pass
+        else:
+            raise exc
+
+
 def infer_current_xenserver_path(repo_name):
     """
     Very hacky for now, cut the path to xenserver-specs
@@ -25,7 +38,43 @@ def infer_current_xenserver_path(repo_name):
         path = cwd.split(repo_name)[0]
         return os.path.normpath("/".join([path, repo_name]))
 
-    raise ValueError("%s not found in %s" % (repo_name, cwd))
+    raise ValueError("Base repository %s not found in the current path %s"
+                     % (repo_name, cwd))
+
+
+def is_spec(repo_path, package_name):
+    """
+    True if repo_path/SPECS/package_name is a spec file,
+    False if it is a link file,
+    Exception("package not present") otherwise
+    """
+    partial_file_path = "%s/SPECS/%s" % (repo_path, package_name)
+
+    if os.path.isfile("%s.spec" % partial_file_path):
+        return True
+    if os.path.isfile("%s.lnk" % partial_file_path):
+        return False
+
+    err_str = "Spec or link file for %s not present in %s/SPECS" % (
+        package_name, repo_path)
+    raise Exception(err_str)
+
+
+def make_pin(xs_path, xs_repos, xs_branch, pinfile_dir, package_name, package_branch):
+    """
+    Return the pinfile path and a dict containing the content of the pinfile
+    """
+    pinfile = {}
+    pinfile['URL'] = "%s/%s" % (xs_repos, package_name)
+    pinfile['commitish'] = package_branch
+    pinfile['patchqueue'] = xs_branch
+
+    if not is_spec(xs_path, package_name):
+        print "%s is a lnk: adding 'specfile' field to the override file" % package_name
+        pinfile['specfile'] = "%s.spec" % package_name
+
+    pinfile_path = "%s/%s.pin" % (pinfile_dir, package_name)
+    return pinfile_path, pinfile
 
 
 def parse_args_or_exit(argv=None):
@@ -35,11 +84,20 @@ def parse_args_or_exit(argv=None):
 
     parser = argparse.ArgumentParser(
         description="Create an override file pointing to a repository \
-                     in xenserver-specs/repos")
+                     in xenserver-specs/repos. The override automatically \
+                     points to the HEAD of the repository.")
     add_common_parser_options(parser)
-    parser.add_argument("reponame", metavar="REPO", help="repository nanme")
-    parser.add_argument("branch", metavar="BRANCH", nargs="?",
-                        help="branch or tag name (defaults to HEAD)")
+    parser.add_argument("packages", metavar="PKG", nargs="+",
+                        help="package name")
+    parser.add_argument("--pinsdir", default="PINS",
+                        help="use custom override folder (default to PINS)")
+    parser.add_argument("--baserepo", default="xenserver-specs",
+                        help="use custom base repository, \
+                              its name must be present in the path \
+                              (dafault to xenserver-specs)")
+    parser.add_argument("--branch", default=None,
+                        help="branch or tag name used for the override \
+                              (defaults to HEAD)")
     argcomplete.autocomplete(parser)
     return parser.parse_args(argv)
 
@@ -51,24 +109,25 @@ def main(argv=None):
 
     args = parse_args_or_exit(argv)
 
-    # TODO: make it customisable
-    xs_path = infer_current_xenserver_path("xenserver-specs")
+    xs_path = infer_current_xenserver_path(args.baserepo)
     xs_branch = current_branch(xs_path)
+    # make it customisable?
+    xs_repos = "repos"
 
-    pinfile = {}
-    pinfile['URL'] = "repos/%s" % args.reponame
-    pinfile['commitish'] = args.branch if args.branch is not None else "HEAD"
-    pinfile['patchqueue'] = xs_branch
-
-    pinfile_dir = "%s/PINS/%s" % (xs_path, xs_branch)
-    pinfile_path = "%s/%s.pin" % (pinfile_dir, args.reponame)
-
-    print(pinfile, pinfile_dir, pinfile_path)
-
+    pinfile_dir = "%s/%s/%s" % (xs_path, args.pinsdir, xs_branch)
     if not os.path.isdir(pinfile_dir):
         print "Creating overrides directory %s" % pinfile_dir
-        os.mkdir(pinfile_dir)
+        mkdir_p(pinfile_dir)
 
-    print "Override file for %s saved in %s" % (args.reponame, pinfile_path)
-    with open(pinfile_path, "w") as pin:
-        json.dump(pinfile, pin)
+    package_branch = args.branch if args.branch is not None else "HEAD"
+
+    for package_name in args.packages:
+        pinfile_path, pinfile = make_pin(xs_path, xs_repos, xs_branch, pinfile_dir,
+                                         package_name, package_branch)
+        with open(pinfile_path, "w") as pin:
+            json.dump(pinfile, pin)
+
+        print "Override file for %s saved in %s with the following content" % (package_name, pinfile_path)
+        print pinfile
+
+    print "To explicitly override the default transformer settings you can run make with PINSDIR=%s" % pinfile_dir
