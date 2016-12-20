17d5d4837f5d7d9d6079a4c43b56d7c0e5641199
Marcello Seri <marcello.seri@citrix.com>
2016-12-20 13:35:32 +0000

    override: really silence pep8
    Signed-off-by: Marcello Seri <marcello.seri@citrix.com>

--- planex/cmd/override.py
+++ planex/cmd/override.py
@@ -27,9 +27,9 @@ def mkdir_p(path):
             raise exc
 
 
-def heuristical_is_spec_repo_root(xs_path):
+def heuristic_is_spec_repo_root(xs_path):
     """
-    Heuristical check for a spec repository root folder.
+    Heuristic check for a spec repository root folder.
     Looks for the presence of xs_path/.git, xs_path/SPECS and xs_path/mock.
     Raise and error if any of those is not present.
     """
@@ -37,8 +37,7 @@ def heuristical_is_spec_repo_root(xs_path):
     if not (os.path.exists("%s/.git" % xs_path) and
             os.path.exists("%s/SPECS" % xs_path) and
             os.path.exists("%s/mock" % xs_path)):
-        raise ValueError("Spec repository not found in the current path: %s"
-                         % xs_path)
+        raise Exception("Spec repository not found in %s" % xs_path)
 
 
 def is_spec(repo_path, package_name):
@@ -59,17 +58,20 @@ def is_spec(repo_path, package_name):
     raise Exception(err_str)
 
 
-def make_pin(xs_path, xs_repos, xs_branch, pinfile_dir, package_name, package_branch):
+def make_pin(xs_path, xs_branch, pinfile_dir, package_name, package_branch):
     """
     Return the pinfile path and a dict containing the content of the pinfile
     """
+
+    xs_repos = "repos"     # make it customisable?
+
     pinfile = {}
     pinfile['URL'] = "%s/%s" % (xs_repos, package_name)
     pinfile['commitish'] = package_branch
     pinfile['patchqueue'] = xs_branch
 
     if not is_spec(xs_path, package_name):
-        print "%s is a lnk: adding 'specfile' field to the override file" % package_name
+        print "%s is a lnk: adding 'specfile' field" % package_name
         pinfile['specfile'] = "%s.spec" % package_name
 
     pinfile_path = "%s/%s.pin" % (pinfile_dir, package_name)
@@ -106,11 +108,9 @@ def main(argv=None):
     args = parse_args_or_exit(argv)
 
     xs_path = os.getcwd()
-    heuristical_is_spec_repo_root(xs_path)
+    heuristic_is_spec_repo_root(xs_path)
 
     xs_branch = current_branch(xs_path)
-    # make it customisable?
-    xs_repos = "repos"
 
     pinfile_dir = "%s/%s/%s" % (xs_path, args.pinsdir, xs_branch)
     if not os.path.isdir(pinfile_dir):
@@ -120,12 +120,14 @@ def main(argv=None):
     package_branch = args.branch if args.branch is not None else "HEAD"
 
     for package_name in args.packages:
-        pinfile_path, pinfile = make_pin(xs_path, xs_repos, xs_branch, pinfile_dir,
+        pinfile_path, pinfile = make_pin(xs_path, xs_branch, pinfile_dir,
                                          package_name, package_branch)
         with open(pinfile_path, "w") as pin:
             json.dump(pinfile, pin)
 
-        print "Override file for %s saved in %s with the following content" % (package_name, pinfile_path)
+        print "Override file for %s saved in %s with the following content" % (
+            package_name, pinfile_path)
         print pinfile
 
-    print "To explicitly override the default transformer settings you can run make with PINSDIR=%s" % pinfile_dir
+    print "To explicitly override the default settings " \
+          "you can run make with PINSDIR=%s" % pinfile_dir
