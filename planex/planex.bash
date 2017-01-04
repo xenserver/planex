720009d1e5aaf1b582a582fd1e5bd10c43d8bdb5
Euan Harris <euan.harris@citrix.com>
2017-01-04 17:39:03 +0000

    bash: Support autocompletion for all command-line utilities
    Signed-off-by: Euan Harris <euan.harris@citrix.com>

--- planex/planex.bash
+++ planex/planex.bash
@@ -1,3 +1,3 @@
-for m in planex-{fetch,patchqueue,depend}; do
+for m in planex-{build-mock,clone-sources,depend,fetch,init,make-srpm,manifest,patchqueue}; do
   eval "$(register-python-argcomplete $m)"
 done
