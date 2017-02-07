xenserver/planex

commit f58e1bf61417e3058f736abd21fc0869e946c029

planex-clone: Allow the list of pin files to be empty
Signed-off-by: Euan Harris <euan.harris@citrix.com>

planex/cmd/clone.py
@@ -4,6 +4,7 @@ planex-clone: Checkout sources referred to by a pin file
 
 from string import Template
 import argparse
+import glob
 import os
 import subprocess
 
@@ -25,11 +26,11 @@ def parse_args_or_exit(argv=None):
     parser.add_argument(
         "-r", "--repos", metavar="DIR", default="repos",
         help='Local path to the repositories')
-    parser.add_argument("pins", metavar="PINS", nargs="+", help="pin file")
+    parser.add_argument("pins", metavar="PINS", nargs="*", help="pin file")
     return parser.parse_args(argv)
 
 
-CHECKOUT_TEMPLATE = Template("""checkout poll: true,
+CHECKOUT_TEMPLATE = Template("""check ut poll: true,
          scm:[$$class: 'GitSCM',
               branches: [[name: '$branch']],
               extensions: [[$$class: 'RelativeTargetDirectory',

commit 5d0a13a3018210d0fc71fd86c7ba5ade9293a98a

planex-clone: Fix typo in template introduced in f58e1bf6
Signed-off-by: Euan Harris <euan.harris@citrix.com>

planex/cmd/clone.py
@@ -29,7 +29,7 @@ def parse_args_or_exit(argv=None):
     return parser.parse_args(argv)
 
 
-CHECKOUT_TEMPLATE = Template("""check ut poll: true,
+CHECKOUT_TEMPLATE = Template("""checkout poll: true,
          scm:[$$class: 'GitSCM',
               branches: [[name: '$branch']],
               extensions: [[$$class: 'RelativeTargetDirectory',