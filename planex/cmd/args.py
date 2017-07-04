8b381519c08c4e162ae8c7ed1567c825fd58423a
Euan Harris <euan.harris@citrix.com>
2017-07-04 13:47:18 +0100

    argparse: Define an argparse type to handle RPM macro definitions
    Signed-off-by: Euan Harris <euan.harris@citrix.com>

--- planex/cmd/args.py
+++ planex/cmd/args.py
@@ -2,6 +2,7 @@
 Common argparse utilities for planex command line tools
 """
 
+import argparse
 import pkg_resources
 
 
@@ -18,3 +19,21 @@ def add_common_parser_options(parser):
                         help='Enable debug logging')
     parser.add_argument('--version', action='version', version="%%(prog)s %s" %
                         pkg_resources.require("planex")[0].version)
+
+
+def rpm_macro(string):
+    """
+    Argparse type handler for RPM macro command line arguments of the form:
+
+       --define "foo bar"
+
+    Returns a (foo, bar) tuple if successful, otherwise raises
+    ArgumentTypeError.
+    """
+    macro = tuple(string.split(' ', 1))
+
+    if len(macro) != 2:
+        msg = "malformed macro passed to --define: %r" % string
+        raise argparse.ArgumentTypeError(msg)
+
+    return macro
