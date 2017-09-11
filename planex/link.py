5d58a101d340f4e1c80c4c99ab42b9553df8b842
Mark Syms <mark.syms@citrix.com>
2017-09-11 13:12:20 +0100

    Teach depend.py how to handle multiple patch URLs
    Signed-off-by: Mark Syms <mark.syms@citrix.com>

--- planex/link.py
+++ planex/link.py
@@ -3,6 +3,14 @@ Classes for dealing with pin and link files
 """
 
 import json
+import re
+
+
+class UnsupportedProperty(RuntimeError):
+    """ Error to be raised if we're asked for properties not present in this
+    schema version
+    """
+    pass
 
 
 class Link(object):
@@ -12,6 +20,12 @@ class Link(object):
         self.path = path
         with open(path) as fileh:
             self.link = json.load(fileh)
+
+    @property
+    def schema_version(self):
+        """Return the schema version of the link file"""
+        # Default to v1 if not present, for legacy lnks
+        return int(self.link.get('SchemaVersion', 1))
 
     @property
     def linkpath(self):
@@ -32,6 +46,8 @@ class Link(object):
     @property
     def patchqueue(self):
         """Return the path to the patchqueue inside the patchqueue tarball"""
+        if self.schema_version > 1:
+            raise UnsupportedProperty('patchqueue only supported on v1')
         return self.link.get('patchqueue', None)
 
     @property
@@ -54,3 +70,34 @@ class Link(object):
         """Return the base repository on top of which to apply the
            patchqueue"""
         return self.link.get('base', None)
+
+    @property
+    def patch_sources(self):
+        """Return the ordered set of patch source definitions"""
+        if self.schema_version < 2:
+            raise UnsupportedProperty('patch_sourcess requries at least'
+                                      'schema version 2')
+
+        patch_matcher = re.compile(r'patch\d+', re.IGNORECASE)
+        return {k: v for k, v
+                in self.link.iteritems()
+                if patch_matcher.match(k)}
+
+    @property
+    def patchqueue_sources(self):
+        """Return the ordered set of patchqueue definitions"""
+        if self.schema_version < 2:
+            raise UnsupportedProperty('patch_sourcess requries at least'
+                                      'schema version 2')
+
+        patch_matcher = re.compile(r'patchqueue\d+', re.IGNORECASE)
+        return {k: v for k, v
+                in self.link.iteritems()
+                if patch_matcher.match(k)}
+
+    @property
+    def has_patches(self):
+        """ Test if the lnk defines patches """
+        return ((self.schema_version == 1 and self.patches is not None) or
+                (self.schema_version >= 2 and
+                 (self.patch_sources or self.patchqueue_sources)))
