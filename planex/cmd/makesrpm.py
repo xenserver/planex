a6b16380bc2547efe195897e547665a08e549c1b
Euan Harris <euan.harris@citrix.com>
2018-02-21 14:25:10 +0000

    makesrpm: Rewrite command to use new spec facilities
    Signed-off-by: Euan Harris <euan.harris@citrix.com>

--- planex/cmd/makesrpm.py
+++ planex/cmd/makesrpm.py
@@ -15,13 +15,9 @@ import tempfile
 import argparse
 import argcomplete
 import planex.cmd.args
-from planex.spec import Spec
+from planex.spec import load
 from planex.link import Link
-from planex.patchqueue import Patchqueue
 from planex.tarball import Tarball
-
-PATCHQUEUES = 'patchqueues'
-PATCHES = 'patches'
 
 
 def parse_args_or_exit(argv=None):
@@ -44,28 +40,6 @@ def parse_args_or_exit(argv=None):
     parsed_args.link = None
     if links:
         parsed_args.link = Link(links[0])
-
-    parsed_args.patchdata = {}
-    if parsed_args.link:
-        link = parsed_args.link
-        if link.schema_version == 1:
-            patchqueues = [arg for arg in argv if arg.endswith("patches.tar")]
-            if patchqueues:
-                parsed_args.patchdata[PATCHES] = patchqueues
-        else:
-            parsed_args.patchdata[PATCHES] = []
-            for patch in link.patch_sources:
-                target = [arg for arg in argv if
-                          arg.endswith('%s.tar' % (patch))]
-                if target:
-                    parsed_args.patchdata[PATCHES].append(target[0])
-
-            parsed_args.patchdata[PATCHQUEUES] = []
-            for patchqueue in link.patchqueue_sources:
-                target = [arg for arg in argv if
-                          arg.endswith('%s.tar' % (patchqueue))]
-                if target:
-                    parsed_args.patchdata[PATCHQUEUES].append(target[0])
 
     return parsed_args
 
@@ -147,88 +121,30 @@ def extract_commit(source, manifests):
                 pass
 
 
-def extract_tarball_patches(tmpdir, spec, tarball, sources, patches):
-    """ Extract a set of patches from a tarball """
-    if sources is not None:
-        for source in spec.local_sources():
-            path = os.path.join(sources, source)
-            tarball.extract(path, tmpdir)
-    if patches is not None:
-        for patch in spec.local_patches():
-            path = os.path.join(patches, patch)
-            tarball.extract(path, tmpdir)
-
-
-def extract_v2_patches(tmpdir, spec, tmp_specfile, link, patchdata):
-    """ Extract patches from a v2 lnk/pin file """
-    if PATCHES in patchdata:
-        patches = patchdata[PATCHES]
-        for patchsource in link.patch_sources:
-            tarname = '%s.tar' % patchsource
-            patchset = [patch for patch in patches
-                        if os.path.basename(patch) == tarname][0]
-            with Tarball(patchset) as tarball:
-                extract_tarball_patches(
-                    tmpdir, spec, tarball,
-                    link.patch_sources[patchsource].get('sources'),
-                    link.patch_sources[patchsource].get('patches'))
-
-    if PATCHQUEUES in patchdata:
-        patchqueues = patchdata[PATCHQUEUES]
-        sources = link.patchqueue_sources
-        for patchqueue in sources:
-            tarname = '%s.tar' % patchqueue
-            patchset = [pq for pq in patchqueues
-                        if os.path.basename(pq) == tarname][0]
-            with Patchqueue(patchset,
-                            branch=sources[patchqueue]
-                            ['patchqueue']) as patches:
-                patches.extract_all(tmpdir)
-                patches.add_to_spec(spec, tmp_specfile)
-
-
-def populate_working_directory(tmpdir, spec, link, sources, patchdata):
+def populate_working_directory(tmpdir, spec):
     """
     Build a working directory containing everything needed to build the SRPM.
     """
-    # Copy spec to working area
-    tmp_specfile = os.path.join(tmpdir, os.path.basename(spec))
-    shutil.copyfile(spec, tmp_specfile)
 
+    sources = [os.path.basename(source[0]) for source in spec.sources()]
+    for source in sources:
+        try:
+            spec.extract_source(source, tmpdir)
+        except KeyError:
+            print("Could not find a source for %s" % source)
+
+    newspec = os.path.join(tmpdir, os.path.basename(spec.specpath()))
     manifests = {}
 
-    # Copy sources to working area
-    for source in sources:
-        extract_commit(source, manifests)
-        shutil.copy(source, tmpdir)
+    with open(newspec, "w") as out:
+        out.writelines(spec.expand_patchqueues())
 
     if manifests:
-        add_manifest_entry(manifests, tmp_specfile)
+        add_manifest_entry(manifests, newspec)
     else:
-        print("No .gitarchive-info found for {0}".format(spec))
+        print("No .gitarchive-info found for {0}".format(spec.name()))
 
-    spec = Spec(tmp_specfile, check_package_name=False)
-
-    # Expand patchqueue to working area, rewriting spec as needed
-    if link:
-        if link.schema_version == 1 and 'patches' in patchdata:
-            patch_path = str(patchdata['patches'][0])
-            # Extract patches
-            if link.patchqueue is not None:
-                print ('patches %s' % (patch_path))
-                with Patchqueue(patch_path,
-                                branch=link.patchqueue) as patches:
-                    patches.extract_all(tmpdir)
-                    patches.add_to_spec(spec, tmp_specfile)
-
-            # Extract non-patchqueue sources
-            with Tarball(patch_path) as tarball:
-                extract_tarball_patches(tmpdir, spec, tarball, link.sources,
-                                        link.patches)
-        elif link.schema_version >= 2:
-            extract_v2_patches(tmpdir, spec, tmp_specfile, link, patchdata)
-
-    return tmp_specfile
+    return newspec
 
 
 def main(argv=None):
@@ -242,8 +158,8 @@ def main(argv=None):
     tmpdir = tempfile.mkdtemp(prefix="px-srpm-")
 
     try:
-        specfile = populate_working_directory(tmpdir, args.spec, args.link,
-                                              args.sources, args.patchdata)
+        spec = load(args.spec, args.link, defines=args.define)
+        specfile = populate_working_directory(tmpdir, spec)
         sys.exit(rpmbuild(args, tmpdir, specfile))
 
     except (tarfile.TarError, tarfile.ReadError) as exc:
