4531df3443e046759a2f032b60df94eec0f4da30
Marcello Seri <marcello.seri@citrix.com>
2018-04-17 11:33:33 +0100

    link.py: cleanup after dropping support for SchemaVersion 1
    Signed-off-by: Marcello Seri <marcello.seri@citrix.com>

--- planex/link.py
+++ planex/link.py
@@ -24,8 +24,13 @@ class Link(object):
     @property
     def schema_version(self):
         """Return the schema version of the link file"""
-        # Default to v1 if not present, for legacy lnks
-        return int(self.link.get('SchemaVersion', 1))
+        schema_version = self.link.get('SchemaVersion', None)
+        if schema_version is None:
+            raise UnsupportedProperty("SchemaVersion field is not present")
+        schema_version = int(schema_version)
+        if schema_version == 1:
+            raise UnsupportedProperty("SchemaVersion 1 is no longer supported")
+        return schema_version
 
     @property
     def ignore_autosetup(self):
@@ -52,45 +57,20 @@ class Link(object):
         return self.link.get('commitish', None)
 
     @property
-    def patchqueue(self):
-        """Return the path to the patchqueue inside the patchqueue tarball"""
-        if self.schema_version > 1:
-            raise UnsupportedProperty('patchqueue only supported on v1')
-        return self.link.get('patchqueue', None)
-
-    @property
     def sources(self):
         """Return the path to extra sources inside the patchqueue tarball"""
-        if self.schema_version < 2:
-            return self.link.get('sources', None)
-
         patch_matcher = re.compile(r'^source\d*$', re.IGNORECASE)
         return {k: v for k, v
                 in self.link.iteritems()
                 if patch_matcher.match(k)}
 
     @property
-    def patches(self):
-        """Return the path to extra patches inside the patchqueue tarball"""
-        return self.link.get('patches', None)
-
-    @property
-    def base_commitish(self):
-        """Return the commitish from which to fetch the patchqueue"""
-        return self.link.get('base_commitish', None)
-
-    @property
-    def base(self):
-        """Return the base repository on top of which to apply the
-           patchqueue"""
-        return self.link.get('base', None)
-
-    @property
     def patch_sources(self):
         """Return the ordered set of patch source definitions"""
-        if self.schema_version < 2:
-            raise UnsupportedProperty('patch_sources requries at least'
-                                      'schema version 2')
+        if self.schema_version > 2:
+            raise UnsupportedProperty(
+                "patchX is supported only in SchemaVersion 2")
+
         patch_matcher = re.compile(r'^patch\d*$', re.IGNORECASE)
         return {k: v for k, v
                 in self.link.iteritems()
@@ -99,10 +79,6 @@ class Link(object):
     @property
     def patchqueue_sources(self):
         """Return the ordered set of patchqueue definitions"""
-        if self.schema_version < 2:
-            raise UnsupportedProperty('patchqueue_sources requries at least'
-                                      'schema version 2')
-
         patch_matcher = re.compile(r'^patchqueue\d*$', re.IGNORECASE)
         return {k: v for k, v
                 in self.link.iteritems()
@@ -111,6 +87,4 @@ class Link(object):
     @property
     def has_patches(self):
         """ Test if the lnk defines patches """
-        return ((self.schema_version == 1 and self.patches is not None) or
-                (self.schema_version >= 2 and
-                 (self.patch_sources or self.patchqueue_sources)))
+        return self.patch_sources or self.patchqueue_sources
