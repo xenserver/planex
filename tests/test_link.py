bceff34fad18c2de7d650ad93999c6ac8404b82d
Marcello Seri <marcello.seri@citrix.com>
2018-04-17 11:56:03 +0100

    test_link.py: update after v1 removal
    Signed-off-by: Marcello Seri <marcello.seri@citrix.com>

--- tests/test_link.py
+++ tests/test_link.py
@@ -17,6 +17,14 @@ class TestLink(unittest.TestCase):
 }
 """  # nopep8
 
+    v1_link_sv = """
+{
+    "SchemaVersion": "1",
+    "URL": "https://code.citrite.net/rest/archive/latest/projects/XS/repos/sm.pg/archive?at=1.14&format=tar#/sm.patches.tar",
+    "patchqueue": "master"
+}
+"""  # nopep8
+
     v2_link = """
 {
     "SchemaVersion": "2",
@@ -41,6 +49,42 @@ class TestLink(unittest.TestCase):
 }
 """  # nopep8
 
+    v3_link = """
+{
+    "SchemaVersion": "3",
+    "Source0":
+    {
+        "URL": "https://code.citrite.net/rest/archive/latest/projects/XSU/repos/sbd/archive?at=v1.3.0&format=tar"
+    },
+    "Source1":
+    {
+        "URL": "Blah"
+    },
+    "Archive0":
+    {
+        "URL": "https://code.citrite.net/rest/archive/latest/projects/~MARKSY/repos/sbd-centos/archive?at=f46b0e608f5&format=tar",
+        "prefix": "SOURCES"
+    },
+    "PatchQueue0":
+    {
+        "URL": "https://code.citrite.net/rest/archive/latest/projects/~MARKSY/repos/sbd.pg/archive?at=f4d3fb5&format=tar",
+        "prefix": "master"
+    }
+}
+"""  # nopep8
+
+    @mock.patch('planex.link.open', mock.mock_open(read_data=v1_link))
+    def test_schema_missing(self):
+        """ Test that the schema version is required """
+        with self.assertRaises(planex.link.UnsupportedProperty):
+            planex.link.Link('test_v1.lnk')
+
+    @mock.patch('planex.link.open', mock.mock_open(read_data=v1_link_sv))
+    def test_schema_v1(self):
+        """ Test that the schema version of a v1 lnk is not supported """
+        with self.assertRaises(planex.link.UnsupportedProperty):
+            planex.link.Link('test_v1.lnk')
+
     @mock.patch('planex.link.open', mock.mock_open(read_data=v2_link))
     def test_schema_v2(self):
         """ Test that the schema version of a v2 lnk is 2 """
@@ -48,32 +92,12 @@ class TestLink(unittest.TestCase):
 
         self.assertEquals(2, link.schema_version)
 
-    @mock.patch('planex.link.open', mock.mock_open(read_data=v1_link))
-    def test_schema_v1(self):
-        """ Test that the schema version of a v1 lnk is coerced to 1 """
-        link = planex.link.Link('test_v1.lnk')
+    @mock.patch('planex.link.open', mock.mock_open(read_data=v3_link))
+    def test_schema_v3(self):
+        """ Test that the schema version of a v3 lnk is 3 """
+        link = planex.link.Link('test_v3.lnk')
 
-        self.assertEquals(1, link.schema_version)
-
-    @mock.patch('planex.link.open', mock.mock_open(read_data=v1_link))
-    def test_patch_sources_v1(self):
-        """ Test that we get an error if asking for patch_sources on v1"""
-
-        link = planex.link.Link('test_v1.lnk')
-        sources = None
-        with self.assertRaises(planex.link.UnsupportedProperty):
-            sources = link.patch_sources
-        self.assertIsNone(sources)
-
-    @mock.patch('planex.link.open', mock.mock_open(read_data=v1_link))
-    def test_patchqueue_sources_v1(self):
-        """ Test that we get an error if asking for patchqueue_sources on v1"""
-
-        link = planex.link.Link('test_v1.lnk')
-        sources = None
-        with self.assertRaises(planex.link.UnsupportedProperty):
-            sources = link.patchqueue_sources
-        self.assertIsNone(sources)
+        self.assertEquals(3, link.schema_version)
 
     @mock.patch('planex.link.open', mock.mock_open(read_data=v2_link))
     def test_patch_sources_v2(self):
@@ -84,6 +108,31 @@ class TestLink(unittest.TestCase):
         self.assertIn('Patch0', sources)
         self.assertNotIn('PatchQueue0', sources)
 
+    @mock.patch('planex.link.open', mock.mock_open(read_data=v3_link))
+    def test_patch_sources_v3(self):
+        """ Test that we get a failure if asking for patch_sources on v3"""
+
+        link = planex.link.Link('test_v3.lnk')
+        with self.assertRaises(planex.link.UnsupportedProperty):
+            _ = link.patch_sources
+
+    @mock.patch('planex.link.open', mock.mock_open(read_data=v2_link))
+    def test_archives_v2(self):
+        """ Test that we get a failure if asking for archives on v2"""
+
+        link = planex.link.Link('test_v2.lnk')
+        with self.assertRaises(planex.link.UnsupportedProperty):
+            _ = link.archives
+
+    @mock.patch('planex.link.open', mock.mock_open(read_data=v3_link))
+    def test_archives_v3(self):
+        """ Test that we get sources if asking for archives on v3"""
+
+        link = planex.link.Link('test_v3.lnk')
+        sources = link.archives
+        self.assertIn('Archive0', sources)
+        self.assertNotIn('PatchQueue0', sources)
+
     @mock.patch('planex.link.open', mock.mock_open(read_data=v2_link))
     def test_patchqueue_sources_v2(self):
         """ Test that we get sources if asking for patchqueue_sources on v2"""
@@ -92,20 +141,10 @@ class TestLink(unittest.TestCase):
         sources = link.patchqueue_sources
         self.assertIn('PatchQueue0', sources)
 
-    @mock.patch('planex.link.open', mock.mock_open(read_data=v1_link))
-    def test_patchqueue_v1(self):
-        """ Test that we get data for patchqueue on v1"""
+    @mock.patch('planex.link.open', mock.mock_open(read_data=v3_link))
+    def test_patchqueue_sources_v3(self):
+        """ Test that we get sources if asking for patchqueue_sources on v2"""
 
-        link = planex.link.Link('test_v1.lnk')
-        patchqueue = link.patchqueue
-        self.assertEqual('master', patchqueue)
-
-    @mock.patch('planex.link.open', mock.mock_open(read_data=v2_link))
-    def test_patchqueue_v2(self):
-        """ Test that we get an error for patchqueue on v2"""
-
-        link = planex.link.Link('test_v2.lnk')
-        patchqueue = None
-        with self.assertRaises(planex.link.UnsupportedProperty):
-            patchqueue = link.patchqueue
-        self.assertIsNone(patchqueue)
+        link = planex.link.Link('test_v3.lnk')
+        sources = link.patchqueue_sources
+        self.assertIn('PatchQueue0', sources)
