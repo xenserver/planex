b9f101b16c51ef3467f1c6f707743389bb486b91
Marcello Seri <marcello.seri@citrix.com>
2018-05-31 16:56:16 +0100

    Prepare to release 4.0.0-beta6
    Signed-off-by: Marcello Seri <marcello.seri@citrix.com>

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ python-setuptools definition for planex
 from setuptools import setup
 
 setup(name='planex',
-      version='4.0.0-beta4',
+      version='4.0.0-beta6',
       packages=['planex', 'planex.cmd'],
       package_data={'planex': ['Makefile.rules']},
       entry_points={
